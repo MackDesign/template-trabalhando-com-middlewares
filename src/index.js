7487846092043c654b360241aa90858c2561d1dd
Mackdesign <marcio@design2u.com.br>
2021-03-15 12:56:05 -0300

    Desafio realizado

--- src/index.js
+++ src/index.js
@@ -10,19 +10,78 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: "User no Found!"});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
+
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro === true || user.todos.length < 10) {
+    return next();
+  } else {
+    return response.status(403).json({
+      error: "Isnt pro plan or have too many to dos",
+    });
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({
+      error: "User not found",
+    });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({
+      error: "ID is not a uuid valid",
+    });
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({
+      error: "Can't find an to do with this ID",
+    });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({
+      error: "User not found",
+    });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
